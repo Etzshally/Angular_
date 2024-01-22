e513ee9c40c0653dd363b56b6cb77a17f3752769
Etzshally <shahpaal10@hotmail.com>
2024-01-22 17:19:03 +0500

    `v1`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule,RouterLink,RouterLinkActive, RouterOutlet, HomeComponent, 
+    AboutComponent, NavbarComponent, 
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
